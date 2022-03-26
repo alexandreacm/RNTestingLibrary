024f2e299153a42bb4c0d416b6a77fefc1184d91
Alexandre carvalho Marques <lexandreacm.marques@gmail.com>
2022-03-26 14:19:05 -0300

    test husky 3.

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-
-import Home from './src/screens/Home';
+import React from 'react'
+import Home from './src/screens/Home'
 
 export default function App() {
-  return <Home />;
+  return <Home />
 }
