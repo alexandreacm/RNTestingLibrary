79f98ac16cc50e9ff678168c06cca3f4d491823d
Alexandre carvalho Marques <lexandreacm.marques@gmail.com>
2022-03-26 14:10:57 -0300

    fix: test husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true
+    es2021: true
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly'
-  },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -19,10 +12,6 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    'no-console': ['error', { allow: ['tron'] }]
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }]
   }
 };
